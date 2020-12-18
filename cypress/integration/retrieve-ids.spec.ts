6a2b8154bf554ead83f9df5a3ac391d7bf0b91d2
Khalfani Wadlington <khalfani@khalfaniw.com>
2020-12-18 02:24:59 +0000

    Created id scraper for youtube homepage with unit+integration tests
    __tests__/scrape-youtube.test.js
    
    __tests__/scrape-youtube.test.js
    
    src/scrape-youtube.test.js
    
    src/scrape-youtube.test.js
    
    src/scrape-youtube.test.js
    
    src/scrape-youtube.test_.js
    
    src/scrape-youtube.test_.js
    
    src/scrape-youtube.test_.js
    
    src/scrape-youtube.test_.js
    
    src/scrape-youtube.test_.js
    
    src/scrape-youtube.test.js
    
    src/scrape-youtube.test.js
    
    src/scrape-youtube.test.js
    
    Created id scraper for youtube homepage with unit+integration tests

---
+++ cypress/integration/retrieve-ids.spec.ts
@@ -0,0 +1,75 @@
+import {ALL_VIDEO_ID_LOCAL_STORAGE_NAME} from '../../src/constants';
+import {
+  appendToAllIdList,
+  retrieveAllIdList,
+  saveAllIdList,
+} from '../../src/store-ids';
+import {
+  extractAllRecommendations,
+  retrievePageHtml,
+} from '../../src/scrape-recommended-ids';
+
+function resetStorage() {
+  window.localStorage.setItem(ALL_VIDEO_ID_LOCAL_STORAGE_NAME, null);
+}
+function retrieveAndUpdateAllVideoIds(html: string = retrievePageHtml()) {
+  const newVideoIds = extractAllRecommendations(html);
+  const updatedAllIdSet = appendToAllIdList(retrieveAllIdList(), newVideoIds);
+  saveAllIdList(updatedAllIdSet);
+}
+function goToYoutubeAndRun(actions: (contentWindow: any) => void) {
+  //readablity is #1 priority
+  cy.visit('https://youtube.com', {
+    onLoad: actions,
+  });
+}
+it('get 1 lists of ids', () => {
+  resetStorage();
+  let done = false;
+  let firstVideoIdCount: number;
+  //first storage is correct
+
+  const checkIfFirstIsCorrect = (window: any) => {
+    const html = window.document.body.innerHTML;
+    retrieveAndUpdateAllVideoIds(html);
+    const newVideoIds = extractAllRecommendations(html);
+    expect(newVideoIds.length).to.equal(50);
+
+    expect(JSON.stringify(retrieveAllIdList())).to.equal(
+      JSON.stringify([newVideoIds]),
+    );
+    firstVideoIdCount = extractAllRecommendations.length;
+
+    /*
+      expect(firstVideoIdCount).to.greaterThan(20);
+      expect(firstVideoIdCount).to.greaterThan(20);
+      expect(firstVideoIdCount).to.lessThan(100);
+    */
+
+    done = true;
+  };
+  goToYoutubeAndRun(checkIfFirstIsCorrect);
+  cy.waitUntil(() => done);
+
+  /*  //check after reloading
+  cy.visit('https://youtube.com', {
+    onLoad: (contentWindow) => {
+      const html = contentWindow.document.body.innerHTML;
+      const secondVideoIds = extractAllRecommendations(html);
+      const secondVideoIdCount = secondVideoIdCount;
+      retrieveAndUpdateAllVideoIds(html);
+      const currentVideoIdCount = retrieveAllIdList().length;
+
+      expect(currentVideoIdCount).to.equal(
+        firstVideoIdCount + secondVideoCount,
+      );
+      const videoCountDiffernce = Math.abs(
+        firstVideoIdCount - secondVideoIdCount,
+      );
+      expect(videoCountDiffernce).to.be.lessThan(10);
+      // console.log(JSON.stringify(currentVideoIdCount))
+      // expect(currentVideoIdCount).to.greaterThan(firstVideoIdCount);
+    },
+  });
+*/
+});
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const ALL_VIDEO_ID_LOCAL_STORAGE_NAME = 'kw_local_storage';
