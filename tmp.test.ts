81248585cea0b7c4972531b510d73c3bdda0fe07
Khalfani Wadlington <khalfani@khalfaniw.com>
2021-03-17 12:03:17 -0500

    Removed useless test

--- tmp.test.ts
+++
@@ -1,8 +0,0 @@
-
-                                  test("tmp5 tpm",  () => {
-                                        let x = 5
-                                    console.log(x);
-                    expect(5).toBe(5)
-                                     });
-                    
-                    
